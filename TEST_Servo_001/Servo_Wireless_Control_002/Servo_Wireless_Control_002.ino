bdb0de40c7808ed317af92051acc6676b1b57f03
Aaron Sin <rockpuncher@icloud.com>
2014-08-14 13:11:02 +0800

    Not working in communicating between arduino

---
+++ TEST_Servo_001/Servo_Wireless_Control_002/Servo_Wireless_Control_002.ino
@@ -0,0 +1,59 @@
+int potPin = A1;
+int servoPin = A0;
+
+boolean ledStatus;
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(potPin, INPUT);
+  pinMode(servoPin, OUTPUT);
+  
+  pinMode(13, OUTPUT);
+  digitalWrite(13, LOW);
+  
+  servoAngle(0);
+  servoAngle(0);
+  servoAngle(0);
+  servoAngle(0);
+  delay(5000);
+}
+
+void loop()
+{
+  int potVal;
+  int n;
+  
+//  potVal = analogRead(potPin);
+  
+  do {
+    potVal = Serial.read();
+  } while (Serial.available() > 0);
+  
+  
+  digitalWrite(13, HIGH);
+  delay(100);
+  digitalWrite(13, LOW);
+  
+  n = map(potVal, 0, 255, -180, 180);
+  
+  servoAngle(n);
+}
+
+void servoAngle(int angle) 
+{
+  int pulseWidth;
+  
+  pulseWidth = map(angle, -180, 180, 220, 2560);
+  digitalWrite(servoPin, HIGH);
+  delayMicroseconds(pulseWidth);
+  
+  digitalWrite(servoPin, LOW);
+  delayMicroseconds(10000 - pulseWidth);
+  
+//  Serial.print("pulseWidth = ");
+//  Serial.print(pulseWidth);
+//  Serial.println("    ");
+//  
+  delay(5);
+}
